33771a419882d649d3b7e2c32f2a99f77243aac5
Nilesh <nilbor407@gmail.com>
2025-05-31 14:49:56 +0530

    new

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
 const app = express();
-
-
+dotenv.config();
 app.get("/",function(req,res){
   res.send("Welcome");
 });
 
-app.listen(8085);
+console.log(process.env.PORT);
+
+app.listen(process.env.PORT);
