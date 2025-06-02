1fbe7aead289b0911c2368e16e1f6e11a2a1a3d5
Nilesh <nilbor407@gmail.com>
2025-06-02 12:13:03 +0530

    again ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-var express = require('express');
-var dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 app.get("/",function(req,res){
