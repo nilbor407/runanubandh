305ca4317a6e26cb01983adec7816082fee399d5
Nilesh <nilbor407@gmail.com>
2025-06-05 11:44:31 +0530

    Update Constants.js

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -7,7 +7,7 @@ const RazorpayKeySecreat = 'DaxxNwrL4Yc2L0P8g1u4617H';
 const salt_key = 'c0f3415e-555f-4019-91ab-a4cdbfc37a1e'; // add salt key which is available in the PhonePay dashboard
 const merchant_id = 'RUNANUBANDHONLINE'; // add merchant_id which is available in the PhonePay dashboard
 const subscriptionAmount = 701;
-const payemntRedirectUrl = 'https://www.runanubandhvishwavivah.com/user/verifyPayment';
+const payemntRedirectUrl = 'https://runanubandh.cloudjiffy.net/user/verifyPayment';
 const frontendPaymentRedirectURL = 'https://runanubandhvishwavivah.com/';
 
 module.exports = {
