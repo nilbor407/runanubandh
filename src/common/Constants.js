8cc916d4ea9680bc6a36c82dc7abf04c4c5d6d3c
Nilesh <nilbor407@gmail.com>
2025-06-05 10:49:27 +0530

    Update Constants.js

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -4,10 +4,10 @@ const RazorpayKeyId = 'rzp_test_zXvhTnlKdx8mfz';
 const RazorpayKeySecreat = 'DaxxNwrL4Yc2L0P8g1u4617H';
 
 //PhonePay
-const salt_key = ''; // add salt key which is available in the PhonePay dashboard
-const merchant_id = ''; // add merchant_id which is available in the PhonePay dashboard
+const salt_key = 'a77cd1fb-00d7-4ce5-b588-95df67f9d105'; // add salt key which is available in the PhonePay dashboard
+const merchant_id = 'RUNANUBANDHONLINE'; // add merchant_id which is available in the PhonePay dashboard
 const subscriptionAmount = 501;
-const payemntRedirectUrl = 'http://localhost:8085/user/verifyPayment';
+const payemntRedirectUrl = 'https://www.runanubandhvishwavivah.com/user/verifyPayment';
 const frontendPaymentRedirectURL = 'https://runanubandhvishwavivah.com/';
 
 module.exports = {
