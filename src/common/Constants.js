816897130f6515ccf8599643dccc08e89c274ad4
Nilesh <59995236+nilbor407@users.noreply.github.com>
2025-11-06 12:16:20 +0530

    Update frontendPaymentRedirectURL for authentication

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -8,7 +8,7 @@ const salt_key = 'c0f3415e-555f-4019-91ab-a4cdbfc37a1e'; // add salt key which i
 const merchant_id = 'RUNANUBANDHONLINE'; // add merchant_id which is available in the PhonePay dashboard
 const subscriptionAmount = 1;
 const payemntRedirectUrl = 'https://runanubandh.cloudjiffy.net/user/verifyPayment';
-const frontendPaymentRedirectURL = 'https://runanubandhvishwavivah.com/';
+const frontendPaymentRedirectURL = 'https://runanubandhvishwavivah.com/auth';
 
 module.exports = {
   JWTScreatKey,
